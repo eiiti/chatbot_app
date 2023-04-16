6d07bc0c25fd2ce689bfe019550ad3b4baad6129
eiiti <52516097+eiiti@users.noreply.github.com>
2023-04-16 22:05:49 +0900

    GPTからの返信が表示されるように修正しました。

--- chatbot_app.py
+++ chatbot_app.py
@@ -2,13 +2,14 @@ import os
 import openai
 import streamlit as st
 
-# APIキーを設定
+# OpenAI APIキーを設定
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# メッセージを入力してGPTからの応答を生成する関数
 def generate_response(message):
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=message,
+        prompt=f"User: {message}\nChatbot:",
         max_tokens=50,
         n=1,
         stop=None,
@@ -17,10 +18,13 @@ def generate_response(message):
     response_text = response.choices[0].text.strip()
     return response_text
 
+# タイトルを設定
 st.title('GPTチャットボット')
 
+# ユーザーからのメッセージを受け取るテキストボックスを作成
 user_message = st.text_input("あなたのメッセージ:")
 
+# 送信ボタンがクリックされたら、GPTからの応答を表示
 if st.button('送信'):
     response = generate_response(user_message)
     st.write(f"チャットボット: {response}")
